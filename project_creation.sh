318bc50d7b91be23f67b590d7fd46f8231585cbc
Aditya <adityak007mr@gmail.com>
2020-01-02 14:57:20 +1100

    Update project_creation.sh

--- project_creation.sh
+++ project_creation.sh
@@ -1,9 +1,9 @@
 # /bin/sh
 
-projectId="cli-project-101"
+projectId="cli-project-102"
 
 #   create the gcp project with the below command
-gcloud projects create $projectId --name="myFirstCliProject" --set-as-default
+gcloud projects create $projectId --name="myNewCliProject" --set-as-default
 
 account_id=$(gcloud alpha billing accounts list --format="value(name)")
 
@@ -15,17 +15,14 @@ gcloud services enable compute.googleapis.com
 
 #   clone the startup-script to a location from the following git repo and use the 
 #   following script to setup the instance
-curl -o worker-startup-script.sh https://raw.githubusercontent.com/ACloudGuru/gcp-cloud-engineer/master/compute-labs/worker-startup-script.sh
+git clone https://github.com/neo7337/gcloudprep.git
 
 #   get the path where the files are cloned and save the path to a variable
 dir=$(pwd)/worker-startup-script.sh
 echo "$dir"
 
 #   create the gcp storage bucket
-gsutil mb gs://bucketcli
+gsutil mb gs://bucketcli2
 
 #   now create the gce instance with the previous startup-script
-gcloud compute instances create cliinstance101 --zone us-central1-a  --machine-type=f1-micro --scopes=default,gke-default,storage-rw --metadata-from-file startup-script=$dir --metadata=lab-logs-bucket=gs://bucketcli
-
-#    to delete an instance
-gcloud compute instances delete cliinstance101 --zone=us-central1-a
+gcloud compute instances create cliinstance101 --zone us-central1-a  --machine-type=f1-micro --scopes=default,gke-default,storage-rw --metadata-from-file startup-script=$dir --metadata=lab-logs-bucket=gs://bucketcli2
