ef5005b4c4f59db88621175ad6cef648278f9b1d
Aditya <adityak007mr@gmail.com>
2020-01-02 17:42:53 +1100

    Update project_creation.sh

--- project_creation.sh
+++ project_creation.sh
@@ -2,7 +2,7 @@
 
 project="createProject-101"
 projectId="mytest-homeproject-101"
-bucketName="testBucket"
+bucketName="myTestProjectBucket"
 instanceGroupName="test-ig"
 instaneTemplate="test-it"
 
@@ -28,7 +28,7 @@ echo "$dir"
 gsutil mb gs://$bucketName
 
 #   creating an instance template
-gcloud beta compute --project=$projectId instance-templates create $instaneTemplate --machine-type=f1-micro --subnet=projects/cli-project-101/regions/us-west1/subnetworks/west1-subnet --network-tier=PREMIUM --metadata=^,@^lab-logs-bucket=gs://$bucketName,@startup-script=$dir --maintenance-policy=MIGRATE --service-account=738364898483-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.write_only --region=us-west1 --tags=open-ssh-tag --image=debian-9-stretch-v20191210 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=$instaneTemplate --labels== --reservation-affinity=any
+gcloud beta compute --project=$project instance-templates create $instaneTemplate --machine-type=f1-micro --subnet=projects/cli-project-101/regions/us-west1/subnetworks/west1-subnet --network-tier=PREMIUM --metadata=^,@^lab-logs-bucket=gs://$bucketName,@startup-script=$dir --maintenance-policy=MIGRATE --service-account=738364898483-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.write_only --region=us-west1 --tags=open-ssh-tag --image=debian-9-stretch-v20191210 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=$instaneTemplate --reservation-affinity=any
 
 #   now create the gce instance-group using the instance template with the previous startup-script
 gcloud beta compute --project=$projectId instance-groups managed create $instanceGroupName --base-instance-name=$instanceGroupName --template=$instaneTemplate --size=1 --zones=us-west2-a,us-west2-b,us-west2-c --instance-redistribution-type=PROACTIVE
